8edd91c4caec384996fcc532ec3a08554a36ed7b
printf2007 <wzxjms@163.com>
2020-01-25 12:03:10 +0800

    Modified the TestFunction.csproj

--- TestFunction/Program.cs
+++ TestFunction/Program.cs
@@ -2,6 +2,8 @@
 
 namespace TestFunction
 {
+
+    /*   //1.1 Simple Test
     class MyClass
     {
        public double Area(double Height,double Width)
@@ -17,6 +19,28 @@ namespace TestFunction
         {
             MyClass mc = new MyClass();
             Console.WriteLine($"the area of height 15,width 20 is {mc.Area(15,20)}");
+        }
+    }
+    */
+
+    //1.2 test return
+    class MyClass
+    {
+        public void TimeUpdate()
+        {
+            int hour = DateTime.Now.Hour;
+            if (hour < 12)
+                return;
+            Console.WriteLine("Hello,It's afternoon");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.TimeUpdate();
         }
     }
 }
